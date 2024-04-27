66fcb1ebce579ba22a46d92e20656a4f826d9a2a
skander23000 <skanderchouiter@gmail.com>
2024-04-27 13:40:10 +0200

    j'ai push la mauvaise version du script pour SOKOBAN

--- SOKOBAN/run_project.sh
+++ SOKOBAN/run_project.sh
@@ -26,11 +26,14 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Exécution de l'application Java..."
+# Récupère le nom du test en paramètre ou utilise 'test1' par défaut
+TEST_NAME="${1:-test1}"
+
+echo "Exécution de l'application Java avec le fichier $TEST_NAME.json..."
 java --add-opens java.base/java.lang=ALL-UNNAMED \
      -server -Xms2048m -Xmx2048m \
      -cp target/sokoban-1.0-SNAPSHOT-jar-with-dependencies.jar \
-     sokoban.SokobanMain test1.json
+     sokoban.SokobanMain "$TEST_NAME.json"
 
 if [ $? -ne 0 ]; then
     echo "L'exécution de l'application Java a échoué."
